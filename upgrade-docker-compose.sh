fb007ddc870c2c757ac8dbfdab90c900448aca69
Keith Moss <keithamoss@gmail.com>
2019-01-03 11:14:06 +0800

    Bumping main docker-compose version and adding upgrade script

---
+++ upgrade-docker-compose.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://stackoverflow.com/a/51435214/7368493
+compose_version=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
+output='/usr/local/bin/docker-compose'
+curl -L https://github.com/docker/compose/releases/download/$compose_version/docker-compose-$(uname -s)-$(uname -m) -o $output
+chmod +x $output
+echo $(docker-compose --version)
