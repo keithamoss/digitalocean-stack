c114f4711e812c646f5d6623dbca6e8682586ca5
Keith Moss <keithamoss@gmail.com>
2019-01-05 17:00:36 +0800

    Nothing to see here

--- upgrade-docker-and-docker-compose.sh
+++ upgrade-docker-and-docker-compose.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Stop the docker containers first!
+# Docker version 17.12.0-ce, build c97c6d6
+apt-get update
+apt-get upgrade docker-ce
+
 # https://stackoverflow.com/a/51435214/7368493
 compose_version=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
 output='/usr/local/bin/docker-compose'
