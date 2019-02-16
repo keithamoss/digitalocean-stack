076ea1b1a0567c99af6fe9b20465a05349ed98c0
Keith Moss <keithamoss@gmail.com>
2019-02-16 10:27:12 +0800

    Add Django static assets to build process

--- prodbuild.sh
+++ prodbuild.sh
@@ -2,7 +2,8 @@
 
 DEMSAUSAGE_VERSION="0.1"
 DEMSAUSAGE_ADMIN_VERSION="0.1"
-SCREMSONG_VERSION="2.1.0"
+DEMSAUSAGE_DJANGO_VERSION="0.1"
+SCREMSONG_VERSION="2.1.6"
 
 \rm -f nginx/build/*.tgz
 mkdir -p nginx/build/scremsong
@@ -15,6 +16,7 @@ mkdir -p nginx/build/demsausage-admin
 cp ../scremsong/build/frontend-"$SCREMSONG_VERSION".tgz ../scremsong/build/django-"$SCREMSONG_VERSION".tgz nginx/build/scremsong
 cp ../demsausage-v3/build/frontend-public-"$DEMSAUSAGE_VERSION".tgz nginx/build/demsausage
 cp ../demsausage-v3/build/frontend-admin-"$DEMSAUSAGE_ADMIN_VERSION".tgz nginx/build/demsausage-admin
+cp ../demsausage-v3/build/django-"$DEMSAUSAGE_DJANGO_VERSION".tgz nginx/build/demsausage-api
 
 echo building prod nginx container
 (cd nginx && docker build -t sausage/nginx:latest .)
