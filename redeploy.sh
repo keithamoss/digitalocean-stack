d21ce17bede2121150f3a9364c0aa5ab0f0b2b90
Keith Moss <keithamoss@gmail.com>
2019-02-17 20:27:44 +0800

    CloudFlare cache is now purged on redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -4,3 +4,6 @@ git pull origin master
 docker-compose -f docker-compose-prod.yml pull
 docker-compose -f docker-compose-prod.yml stop
 docker-compose -f docker-compose-prod.yml up -d
+
+export $(xargs < secrets/travis.env)
+curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email:$CF_EMAIL" -H "X-Auth-Key:$CF_API_KEY" -H "Content-Type:application/json" --data '{"purge_everything":true}'
