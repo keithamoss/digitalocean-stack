d94212f45362cb37c74cb2bb0c0afd7843f6fd95
Keith Moss <keithamoss@gmail.com>
2019-03-04 17:34:19 +0800

    Separate Scremsong and Democracy Sausage (different droplets)

--- prodbuild.sh
+++ prodbuild.sh
@@ -4,9 +4,11 @@ DEMSAUSAGE_VERSION="0.2.2"
 DEMSAUSAGE_ADMIN_VERSION="0.2.2"
 DEMSAUSAGE_DJANGO_VERSION="0.2.2"
 SCREMSONG_VERSION="2.1.6"
+SCREMSONG_DJANGO_VERSION="2.1.6"
 
 \rm -f nginx/build/*.tgz
 mkdir -p nginx/build/scremsong
+mkdir -p nginx/build/scremsong-api
 mkdir -p nginx/build/demsausage
 mkdir -p nginx/build/demsausage-admin
 mkdir -p nginx/build/demsausage-api
@@ -15,6 +17,7 @@ mkdir -p nginx/build/demsausage-api
 # assumes local sources exist for DemocracySausage and Scremsong
 # this is horrible, fixme
 cp ../scremsong/build/frontend-"$SCREMSONG_VERSION".tgz ../scremsong/build/django-"$SCREMSONG_VERSION".tgz nginx/build/scremsong
+cp ../scremsong/build/django-"$SCREMSONG_DJANGO_VERSION".tgz nginx/build/scremsong-api
 cp ../demsausage/build/frontend-public-"$DEMSAUSAGE_VERSION".tgz nginx/build/demsausage
 cp ../demsausage/build/frontend-admin-"$DEMSAUSAGE_ADMIN_VERSION".tgz nginx/build/demsausage-admin
 cp ../demsausage/build/django-"$DEMSAUSAGE_DJANGO_VERSION".tgz nginx/build/demsausage-api
---
+++ redeploy-scremsong.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git pull origin master
+docker-compose -f docker-compose-prod-scremsong.yml pull
+docker-compose -f docker-compose-prod-scremsong.yml stop
+docker-compose -f docker-compose-prod-scremsong.yml up -d
+
+export $(xargs < secrets/travis.env)
+curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email:$CF_EMAIL" -H "X-Auth-Key:$CF_API_KEY" -H "Content-Type:application/json" --data '{"purge_everything":true}'
