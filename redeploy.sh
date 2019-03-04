db2fd3205d81182169eff07f7c5e78319babcb5f
Keith Moss <keithamoss@gmail.com>
2019-03-04 18:00:40 +0800

    Last step of separating out the DemSausage and Scremsong droplets

--- redeploy.sh
+++ redeploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-git pull origin master
-docker-compose -f docker-compose-prod.yml pull
-docker-compose -f docker-compose-prod.yml stop
-docker-compose -f docker-compose-prod.yml up -d
+echo "Nothing to see here."
 
-export $(xargs < secrets/travis.env)
-curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email:$CF_EMAIL" -H "X-Auth-Key:$CF_API_KEY" -H "Content-Type:application/json" --data '{"purge_everything":true}'
+# git pull origin master
+# docker-compose -f docker-compose-prod.yml pull
+# docker-compose -f docker-compose-prod.yml stop
+# docker-compose -f docker-compose-prod.yml up -d
+
+# export $(xargs < secrets/travis.env)
+# curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email:$CF_EMAIL" -H "X-Auth-Key:$CF_API_KEY" -H "Content-Type:application/json" --data '{"purge_everything":true}'
