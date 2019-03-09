fd9460287adf43e7c12b106862fb6ce91eff6800
Keith Moss <keithamoss@gmail.com>
2019-03-09 19:24:41 +0800

    Removing rabbitmq

--- redeploy-demsausage.sh
+++ redeploy-demsausage.sh
@@ -3,7 +3,7 @@
 git pull origin master
 docker-compose -f docker-compose-prod-demsausage.yml pull
 docker-compose -f docker-compose-prod-demsausage.yml stop
-docker-compose -f docker-compose-prod-demsausage.yml up -d
+docker-compose -f docker-compose-prod-demsausage.yml up --remove-orphans -d
 
 export $(xargs < secrets/travis.env)
 curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email:$CF_EMAIL" -H "X-Auth-Key:$CF_API_KEY" -H "Content-Type:application/json" --data '{"purge_everything":true}'
