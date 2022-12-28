e47965a6d86ae1717c8aa5db0529f42bef77fdad
Keith Moss <keithamoss@gmail.com>
2022-12-28 19:51:51 +0800

    DS prod env

---
+++ redeploy-demsausage-production.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git pull origin master
+docker compose -f demsausage-production.yml pull
+docker compose -f demsausage-production.yml stop
+docker compose -f demsausage-production.yml up --remove-orphans -d
+
+export $(xargs < secrets/cloudflare.env)
+curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email:$CF_EMAIL" -H "X-Auth-Key:$CF_API_KEY" -H "Content-Type:application/json" --data '{"purge_everything":true}'
