83e5cdb34a63a19b4083ac0723c16f6c958b6099
Keith Moss <keithamoss@gmail.com>
2022-12-28 14:28:08 +0800

    DemSausage staging env WIP

---
+++ redeploy-demsausage-staging.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git pull origin master
+docker-compose -f demsausage-staging.yml pull
+docker-compose -f demsausage-staging.yml stop
+docker-compose -f demsausage-staging.yml up --remove-orphans -d
+
+export $(xargs < secrets/travis.env)
+curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email:$CF_EMAIL" -H "X-Auth-Key:$CF_API_KEY" -H "Content-Type:application/json" --data '{"purge_everything":true}'
