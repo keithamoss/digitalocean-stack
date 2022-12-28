99e61014b69443b12323dc847011da5de40f3ae7
Keith Moss <keithamoss@gmail.com>
2022-12-28 21:14:24 +0800

    Scremsong envs and new build scripts

--- redeploy-scremsong-production.sh
+++ redeploy-scremsong-production.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 git pull origin master
-docker-compose -f docker-compose-prod-scremsong.yml pull
-docker-compose -f docker-compose-prod-scremsong.yml stop
-docker-compose -f docker-compose-prod-scremsong.yml up --remove-orphans -d
+docker compose -f scremsong-production.yml pull
+docker compose -f scremsong-production.yml stop
+docker compose -f scremsong-production.yml up --remove-orphans -d
 
-export $(xargs < secrets/travis.env)
+docker image prune --force
+
+export $(xargs < secrets/cloudflare.env)
 curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email:$CF_EMAIL" -H "X-Auth-Key:$CF_API_KEY" -H "Content-Type:application/json" --data '{"purge_everything":true}'
