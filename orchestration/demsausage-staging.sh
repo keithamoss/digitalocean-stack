c279060a4d1c0cf399f54546e82ad5698dd229b4
Keith Moss <keithamoss@gmail.com>
2025-12-29 19:00:53 +0800

    Adding CloudFlare cache busting back in

--- orchestration/demsausage-staging.sh
+++ orchestration/demsausage-staging.sh
@@ -6,3 +6,6 @@ docker compose -f ../demsausage/staging.yml stop
 docker compose -f ../demsausage/staging.yml up --remove-orphans -d
 
 docker image prune --force
+
+export $(xargs < ./secrets/cloudflare.env)
+curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email:$CF_EMAIL" -H "X-Auth-Key:$CF_API_KEY" -H "Content-Type:application/json" --data '{"purge_everything":true}'
